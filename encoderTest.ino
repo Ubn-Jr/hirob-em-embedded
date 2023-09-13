0cf3ea99955830f0cd2cbbc179db04dd25454112
yavuz-yusuf <142804723+yavuz-yusuf@users.noreply.github.com>
2023-09-13 18:24:59 +0300

    Create encoderTest.ino

---
+++ encoderTest.ino
@@ -0,0 +1,51 @@
+#define MotFwdL  23
+#define MotRevL  22
+
+
+const int encoderPin1A = 27;  // Encoder pin A
+const int encoderPin1B = 26;  // Encoder pin B
+
+
+volatile long encoder1Value = 0;
+
+
+void setup() {
+
+  Serial.begin(9600);
+
+
+  pinMode(MotFwdL, OUTPUT);
+  pinMode(MotRevL, OUTPUT);
+  Serial.begin(9600);
+
+
+  pinMode(encoderPin1A, INPUT_PULLUP);
+  pinMode(encoderPin1B, INPUT_PULLUP);
+
+
+  attachInterrupt(digitalPinToInterrupt(encoderPin1A), updateEncoder1, CHANGE);
+
+}
+
+void loop() {
+
+
+ digitalWrite(MotFwdL, HIGH);
+ digitalWrite(MotRevL, LOW);
+ Serial.print("Forward  ");
+
+
+
+  Serial.print("  Encoder 1: ");
+  Serial.println(encoder1Value);
+
+}
+
+
+void updateEncoder1() {
+  if (digitalRead(encoderPin1A) == digitalRead(encoderPin1B)) {
+    encoder1Value++;
+  } else {
+    encoder1Value--;
+  }
+}
